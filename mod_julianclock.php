a3000421b7de0f941c0fee8c8ff7861cde3153c4
N6REJ <troy@hallhome.us>
2016-04-24 11:26:36 -0500

    working!!!  Tested against Nasa's clock.

--- mod_julianclock.php
+++ mod_julianclock.php
@@ -2,18 +2,11 @@
 
 /**
  * @package julianClock for Joomla 3.0
- * @version 1.0
+ * @version 1.2
  * @author Troy T. Hall (http://jowwow.net)
  * @copyright (C) 2016 JowWow
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  * */
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-
-require_once( dirname(__FILE__) . '/helper.php' );
-
-// Get default .css file
-$document = JFactory::getDocument();
-$document->addStyleSheet(JURI::base() . 'modules/mod_julianclock/css/mod_julianclock.css');
-$julianDate = modJulianClockHelper::getTime($params);
 require( JModuleHelper::getLayoutPath('mod_julianclock') );
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package julianclock for Joomla 3.0
- * @version 1.0
+ * @version 1.2
  * @author Troy T. Hall (http://jowwow.me)
  * @copyright (C) 2013 JowWow
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -16,29 +16,45 @@ $moduleSuffix = $params->get('moduleclass_sfx');
 ?>
 <!-- BEGIN LAYOUT -->
 
-<div class="<?php echo "julianclock_" . $moduleTitle . '"';
+<div class="<?php
+echo "julianclock_" . $moduleTitle . '"';
 if ($moduleSuffix) {
 	echo 'class=" ' . $moduleSuffix . '">';
 } else {
 	echo ">";
 }
 ?>
-</div>
+	 </div>
 	 <!-- END LAYOUT -->
+	 <!-- formula derived from https://en.wikipedia.org/wiki/Julian_day -->
 	 <script type="text/javascript" >
-	 function julianclockUpdate_<?php echo $moduleTitle; ?>()
+
+	 function getJulian_<?php echo $moduleTitle; ?>()
 	 {
+	 var now = new Date();
+	 var day = now.getUTCDate();
+	 var month = now.getUTCMonth()+1;
+	 var year = now.getUTCFullYear();
+	 var hours = now.getUTCHours();
+	 var minutes = now.getUTCMinutes();
+	 var seconds = now.getUTCSeconds();
+	 var a = Math.floor((14 - month) / 12);
+	 var y = year + 4800 - a;
+	 var m = month + (12 * a) - 3;
+	 var JDN = day + Math.floor(((153 * m) + 2) / 5) + (365 * y) + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) - 32045;
+	 var JulianDate = JDN + ((hours - 12) / 24) + (minutes / 1440) + (seconds/86400);
+
 	 if(<?php echo $params->get('positionText'); ?> == 1) {
-	 jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(<?php echo $julianDate; ?>).prepend('<?php echo $params->get('clockText'); ?> ');
+	 jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(JulianDate).prepend('<?php echo $params->get('clockText'); ?> ');
 	 } else	 {
-	 jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(<?php echo $julianDate; ?>).append(' <?php echo $params->get('clockText'); ?>');
+	 jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(JulianDate).append(' <?php echo $params->get('clockText'); ?>');
 	 }
 	 }
 
 	 // Update the time display
 	 jQuery(document).ready(function ()
 	 {
-	 setInterval('julianclockUpdate_<?php echo $moduleTitle; ?>()', <?php echo $params->get('updateInterval'); ?>);
+	 setInterval(getJulian_<?php echo $moduleTitle; ?>, <?php echo $params->get('updateInterval'); ?>);
 	 });
 
 </script>
