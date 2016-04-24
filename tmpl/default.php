b4edfff808cd328760541909f66bbbb459c045dc
N6REJ <troy@hallhome.us>
2016-04-24 07:50:18 -0500

    cleaned up text display

--- tmpl/default.php
+++ tmpl/default.php
@@ -16,30 +16,29 @@ $moduleSuffix = $params->get('moduleclass_sfx');
 ?>
 <!-- BEGIN LAYOUT -->
 
-<div class="<?php
-echo "julianclock_" . $moduleTitle . '"';
+<div class="<?php echo "julianclock_" . $moduleTitle . '"';
 if ($moduleSuffix) {
 	echo 'class=" ' . $moduleSuffix . '">';
 } else {
 	echo ">";
 }
 ?>
-	 </div>
+</div>
 	 <!-- END LAYOUT -->
 	 <script type="text/javascript" >
 	 function julianclockUpdate_<?php echo $moduleTitle; ?>()
 	 {
-	 if(<?php echo $params->get('positionText'); ?>) {
-		jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(<?php echo $julianDate; ?>).prepend('<?php echo $params->get('clockText'); ?> ');
+	 if(<?php echo $params->get('positionText'); ?> == 1) {
+	 jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(<?php echo $julianDate; ?>).prepend('<?php echo $params->get('clockText'); ?> ');
 	 } else	 {
-		jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(<?php echo $julianDate; ?>).append(' <?php echo $params->get('clockText'); ?>');
+	 jQuery(".julianclock_<?php echo $moduleTitle; ?>").html(<?php echo $julianDate; ?>).append(' <?php echo $params->get('clockText'); ?>');
 	 }
 	 }
 
 	 // Update the time display
 	 jQuery(document).ready(function ()
 	 {
-	 setInterval('julianclockUpdate_<?php echo $moduleTitle; ?>()', 1000);
+	 setInterval('julianclockUpdate_<?php echo $moduleTitle; ?>()', <?php echo $params->get('updateInterval'); ?>);
 	 });
 
 </script>
