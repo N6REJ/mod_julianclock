e4c20a1da59158db9550da0ee0673cbee14010da
N6REJ <troy@hallhome.us>
2016-04-27 18:50:55 -0500

    rewrite to make things more efficient based on Hannes recommendations.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 JulianClock
 ========
-A live-clock based on Javascript for Joomla 3.
+A live-clock based on Javascript & php for Joomla 3.
 while this may run on Joomla 2.5 no support is given for such.
 
 Source Time is fixed on UTC
+deltaT is not utilized in this clock
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package julianclock for Joomla 3.0
- * @version 1.2
+ * @package jdate for Joomla 3.0
+ * @version 1.5
  * @author Troy T. Hall (http://jowwow.me)
  * @copyright (C) 2013 JowWow
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -14,46 +14,53 @@ $moduleTitle = strtolower($moduleTitle);
 $moduleTitle = preg_replace('/[^a-z0-9]/i', '_', $moduleTitle);
 $moduleSuffix = $params->get('moduleclass_sfx');
 
+/* lets set the text for pre and post time */
+if ($params->get('position')) {
+	$preText = $params->get('clockText') . " ";
+	$postText = '';
+} else {
+	$postText = $params->get('clockText') . " ";
+	$preText = '';
+}
+
 // Construct the CSS class string.
 $baseClass = 'jdate_' . $moduleTitle;
-$classes = $baseClass . ($moduleSuffix ? ' ' . $baseClass . $moduleSuffix : '');
-
+$classes = $baseClass . ($moduleSuffix ? ' ' . $moduleSuffix : ' ');
 ?>
+
 <!-- BEGIN LAYOUT -->
-	<div class="<?php echo $classes; ?>">
-	</div>
+<div class="<?php echo $classes; ?>">
+</div>
 <!-- END LAYOUT -->
 
-	 <!-- formula derived from https://en.wikipedia.org/wiki/Julian_day -->
-	 <script type="text/javascript" >
+<!-- formula derived from https://en.wikipedia.org/wiki/Julian_day -->
+<script type="text/javascript" >
 
-	 function getJulian_<?php echo $classes; ?>()
-	 {
-	 var now = new Date();
-	 var day = now.getUTCDate();
-	 var month = now.getUTCMonth()+1;
-	 var year = now.getUTCFullYear();
-	 var hours = now.getUTCHours();
-	 var minutes = now.getUTCMinutes();
-	 var seconds = now.getUTCSeconds();
-	 var a = Math.floor((14 - month) / 12);
-	 var y = year + 4800 - a;
-	 var m = month + (12 * a) - 3;
-	 var JDN = day + Math.floor(((153 * m) + 2) / 5) + (365 * y) + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) - 32045;
-	 var JulianDate = JDN + ((hours - 12) / 24) + (minutes / 1440) + (seconds/86400);
+	function getJulian_<?php echo $moduleTitle; ?>()
+	{
+		var now = new Date();
+		var day = now.getUTCDate();
+		var month = now.getUTCMonth() + 1;
+		var year = now.getUTCFullYear();
+		var hours = now.getUTCHours();
+		var minutes = now.getUTCMinutes();
+		var seconds = now.getUTCSeconds();
+		var a = Math.floor((14 - month) / 12);
+		var y = year + 4800 - a;
+		var m = month + (12 * a) - 3;
+		var JDN = day + Math.floor(((153 * m) + 2) / 5) + (365 * y) + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) - 32045;
+		var JulianDate = JDN + ((hours - 12) / 24) + (minutes / 1440) + (seconds / 86400);
 
-	 if(<?php echo $params->get('positionText'); ?> == 1) {
-	 jQuery(".<?php echo $classes; ?>").html(JulianDate).prepend('<?php echo $params->get('clockText'); ?> ');
-	 } else	 {
-	 jQuery(".<?php echo $classes; ?>").html(JulianDate).append(' <?php echo $params->get('clockText'); ?>');
-	 }
-	 }
+		jQuery(".jdate_<?php echo $moduleTitle; ?>").html('<?php echo $preText; ?>' + JulianDate + '<?php echo $postText; ?>');
+	}
 
-	 // Update the time display
-	 jQuery(document).ready(function ()
-	 {
-		 getJulian_<?php echo $classes; ?>();
-	 setInterval(getJulian_<?php echo $classes; ?>, <?php echo $params->get('updateInterval'); ?>);
-	 });
+	// Lets call the clock the so display it the first time before the counter starts
+	getJulian_<?php echo $moduleTitle; ?>();
+
+	// Update the displayed time after x period
+	jQuery(document).ready(function ()
+	{
+		setInterval(getJulian_<?php echo $moduleTitle; ?>, <?php echo $params->get('updateInterval'); ?>);
+	});
 
 </script>
